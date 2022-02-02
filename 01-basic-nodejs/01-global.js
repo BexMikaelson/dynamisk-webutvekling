fb991ced8457a075f6916a313a9ae2220c34403a
Heidi <heidi@AirsomthorHeidi.lan>
2022-02-02 11:29:58 +0100

    first commit

---
+++ 01-basic-nodejs/01-global.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+
+setTimeout(() => {
+    console.log("I'll be back!");
+
+}, 3000);
+
+
+let counter = 0;
+setInterval(() => {
+    counter += 2;
+    console.log(`Hello after ${counter} seconds`);
+}, 2000);
