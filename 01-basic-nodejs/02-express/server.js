1a3cb2393633269beb24247bf994516fdc1ab0bd
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-02-02 13:58:05 +0100

    adding respond to GET-requests

--- 01-basic-nodejs/02-express/server.js
+++ 01-basic-nodejs/02-express/server.js
@@ -3,6 +3,36 @@ const express = require('express');
 
 const app = express();
 
+
+//respond to GET request for `/`
+app.get('/',(req,res) =>{
+    // req = info om den inkommande förfrågan
+    // res = metoder för att skicka ett svar på förfrågan
+    console.log("I Am Groot!");
+
+    res.send('Hello');
+});
+
+// respond to GET-request for `/nom`
+app.get('nom',(req,res) =>{
+    console.log("somone new");
+
+    res.send('Hello yes to Max homade cupcakes');
+});
+
+//respond to GET request for `/api/nom`
+app.get(`/api/nom`, (req,res)=>{
+    res.send({msg:'Cakes are nom-nom-nom.'});
+});
+
+//respond to GET request for `/about`
+app.get('/about',(req,res) =>{
+   res.send("<h1>I see you<h1>");
+   res.send("<p>nubbb<p>");
+});
+
+// start listening for incoming requests on port 3000
 app.listen(3000,()=>{
     console.log (`yey`);
 });
+
